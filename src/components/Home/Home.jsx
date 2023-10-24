a5b71d1fc7bc2ea5144d2d9a74185be097c124b0
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-10-24 18:09:43 +0300

    Added components and dispatch which helps to filter the price less 100 using UseEffect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Poster from "../Poster/Poster";
+import Products from "../Products/Products";
+import { useDispatch, useSelector } from "react-redux";
+import Categories from "../Categories/Categories";
+import Banner from "../Banner/Banner";
+import { filteredByPrice } from "../../features/products/productsSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const {
+    products: { list, filtered },
+    categories,
+  } = useSelector((state) => ({
+    products: state.products,
+    categories: state.categories,
+  }));
+
+  useEffect(() => {
+    if (!list.length) return;
+
+    dispatch(filteredByPrice(100));
+  }, [dispatch, list.length]);
+
   return (
     <>
-      <h1>Home</h1>
+      <Poster />
+      <Products products={list} amount={10} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Categories" />
+      <Banner />
+      <Products products={filtered} amount={5} title="Less than 100$" />
     </>
   );
 };
