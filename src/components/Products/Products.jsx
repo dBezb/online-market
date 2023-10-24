946f7541efd4faaa658acd971757b579508ca8cb
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-10-24 18:11:21 +0300

    FIX:Title

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,35 +4,37 @@ import { Link } from "react-router-dom";
 
 const Products = ({ title, style = {}, products = [], amount }) => {
   const list = products.filter((_, i) => i < amount);
-
+  const firstTenthItems = list.slice(0, 10);
   return (
     <section className={styles.products} style={style}>
+      {title && <h2>{title}</h2>}
       <div className={styles.list}>
-        {title && <h2>{title}</h2>}
-        {list.map(({ id, images, title, category: { name: cat }, price }) => (
-          <Link to={`products/${id}`} key={id} className={styles.product}>
-            <div
-              className={styles.image}
-              style={{ backgroundImage: `url(${images[0]})` }}
-            />
+        {firstTenthItems.map(
+          ({ id, images, title, category: { name: cat }, price }) => (
+            <Link to={`/products/${id}`} key={id} className={styles.product}>
+              <div
+                className={styles.image}
+                style={{ backgroundImage: `url(${images[0]})` }}
+              />
 
-            <div className={styles.wrapper}>
-              <h3 className={styles.title}>{title}</h3>
-              <div className={styles.cat}>{cat}</div>
-              <div className={styles.info}>
-                <div className={styles.prices}>
-                  <div className={styles.price}>{price}$</div>
-                  <div className={styles.oldPrice}>
-                    {Math.floor(price * 0.7)}$
+              <div className={styles.wrapper}>
+                <h3 className={styles.title}>{title}</h3>
+                <div className={styles.cat}>{cat}</div>
+                <div className={styles.info}>
+                  <div className={styles.prices}>
+                    <div className={styles.price}>{price}$</div>
+                    <div className={styles.oldPrice}>
+                      {Math.floor(price * 0.7)}$
+                    </div>
+                  </div>
+                  <div className={styles.purchases}>
+                    {Math.floor(Math.random() * 20 + 1)} purchased
                   </div>
                 </div>
-                <div className={styles.purchases}>
-                  {Math.floor(Math.random() * 20 + 1)} purchased
-                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          )
+        )}
       </div>
     </section>
   );
