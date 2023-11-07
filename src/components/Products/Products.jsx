4cfc2115e39c333906888d4af888bdb60614fd0f
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-11-07 22:19:00 +0200

    REFACTOR: Refactored the code

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,37 +4,33 @@ import { Link } from "react-router-dom";
 
 const Products = ({ title, style = {}, products = [], amount }) => {
   const list = products.filter((_, i) => i < amount);
-  const firstTenthItems = list.slice(10, 20);
+  // const firstTenthItems = list.slice(10, 20);
   return (
     <section className={styles.products} style={style}>
       {title && <h2>{title}</h2>}
       <div className={styles.list}>
-        {firstTenthItems.map(
-          ({ id, images, title, category: { name: cat }, price }) => (
-            <Link to={`/products/${id}`} key={id} className={styles.product}>
-              <div
-                className={styles.image}
-                style={{ backgroundImage: `url(${images[0]})` }}
-              />
+        {list.map(({ id, images, title, category: { name: cat }, price }) => (
+          <Link to={`/products/${id}`} key={id} className={styles.product}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: `url(${images[0]})` }}
+            />
 
-              <div className={styles.wrapper}>
-                <h3 className={styles.title}>{title}</h3>
-                <div className={styles.cat}>{cat}</div>
-                <div className={styles.info}>
-                  <div className={styles.prices}>
-                    <div className={styles.price}>
-                      {Math.floor(price * 0.8)}$
-                    </div>
-                    <div className={styles.oldPrice}>{price}$</div>
-                  </div>
-                  <div className={styles.purchases}>
-                    {Math.floor(Math.random() * 20 + 1)} purchased
-                  </div>
+            <div className={styles.wrapper}>
+              <h3 className={styles.title}>{title}</h3>
+              <div className={styles.cat}>{cat}</div>
+              <div className={styles.info}>
+                <div className={styles.prices}>
+                  <div className={styles.price}>{Math.floor(price * 0.8)}$</div>
+                  <div className={styles.oldPrice}>{price}$</div>
+                </div>
+                <div className={styles.purchases}>
+                  {Math.floor(Math.random() * 20 + 1)} purchased
                 </div>
               </div>
-            </Link>
-          )
-        )}
+            </div>
+          </Link>
+        ))}
       </div>
     </section>
   );
