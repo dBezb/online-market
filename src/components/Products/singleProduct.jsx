c34bc307118cdc04f6c2a301f1401ac685ed5269
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-11-07 22:23:11 +0200

    FEAT: Added Related products section & and functionality for it using useDispatch & useSelector

--- src/components/Products/singleProduct.jsx
+++ src/components/Products/singleProduct.jsx
@@ -3,10 +3,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useGetProductQuery } from "../../features/api/apiSlice";
 import { ROUTES } from "../../utils/routes";
 import Product from "./Product";
+import { useDispatch, useSelector } from "react-redux";
+import { getRelaredProducts } from "../../features/products/productsSlice";
+import Products from "./Products";
 
 const SingleProduct = () => {
+  const dispatch = useDispatch();
   const { id } = useParams();
   const navigate = useNavigate();
+  const { list, related } = useSelector(({ products }) => products);
 
   const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id });
 
@@ -16,14 +21,21 @@ const SingleProduct = () => {
     }
   }, [isLoading, isFetching, isSuccess]);
 
+  useEffect(() => {
+    if (!data || !list.length) return;
+
+    dispatch(getRelaredProducts(data.category.id));
+  }, [data, dispatch, list.length]);
+
   console.log(data);
 
   return !data ? (
     <section className="preloader">Loading...</section>
   ) : (
-    <div>
+    <>
       <Product {...data} />
-    </div>
+      <Products products={related} amount={5} title="Related products" />
+    </>
   );
 };
 
