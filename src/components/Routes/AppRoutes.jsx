91b2bec1e8f19f8e5e129346e6428c67373986b7
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-11-15 15:48:25 +0200

    FEAT:Added a new Category Route

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Home from "../Home/Home";
 import { ROUTES } from "../../utils/routes";
 import SingleProduct from "../Products/singleProduct";
 import Profile from "../Profile/Profile";
+import SingleCategory from "../Categories/SingleCategory";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
       <Route index element={<Home />} />
       <Route path={ROUTES.PRODUCT} element={<SingleProduct />}></Route>
       <Route path={ROUTES.PROFILE} element={<Profile />}></Route>
+      <Route path={ROUTES.CATEGORY} element={<SingleCategory />}></Route>
     </Routes>
   );
 };
