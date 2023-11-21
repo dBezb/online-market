d89bca29fd3b1b1d1960a9a1b37427eca8f5081c
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-11-21 16:27:57 +0200

    FEAT:Created functionality for displaying products in different categories

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -2,59 +2,139 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../../features/api/apiSlice";
 import styles from "../../styles/Category.module.css";
+import Products from "../Products/Products";
+import { useSelector } from "react-redux";
 
 const Category = () => {
   const { id } = useParams();
+  const { list } = useSelector(({ categories }) => categories);
 
-  const defaultParams = {
+  const defaultValues = {
     title: "",
     price_min: 0,
     price_max: 0,
-    categoryId: "",
   };
 
+  const defaultParams = {
+    categoryId: "",
+    limit: 10,
+    offset: 0,
+    ...defaultValues,
+  };
+
+  const [isEnd, setIsEnd] = useState(false);
+  const [cat, setCat] = useState(null);
+  const [items, setItems] = useState([]);
+  const [values, setValues] = useState(defaultValues);
   const [params, setParams] = useState(defaultParams);
+
+  const { data, isLoading, isSuccess } = useGetProductsQuery(params);
 
   useEffect(() => {
     if (!id) return;
+
+    setValues(defaultValues);
+    setItems([]);
+    setIsEnd(false);
     setParams({ ...defaultParams, categoryId: id });
   }, [id]);
 
-  const { data } = useGetProductsQuery(params);
+  useEffect(() => {
+    if (isLoading) return;
+    if (!data.length) return setIsEnd(true);
+
+    const products = Object.values(data);
+
+    if (!products.length) return;
+
+    setItems((_items) => [..._items, ...products]);
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    if (!id || !list.length) return;
+
+    const category = list.find((item) => item.id === id * 1);
+
+    setCat(category);
+  }, [list, id]);
+
+  const handleChange = ({ target: { value, name } }) => {
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setItems([]);
+    setIsEnd(false);
+
+    setParams({ ...defaultParams, ...values });
+  };
 
   return (
     <section className={styles.wrapper}>
-      <h2 className={styles.title}>Shoes</h2>
+      <h2 className={styles.title}>{cat?.name}</h2>
 
-      <form className={styles.filters} onSubmit={() => {}}>
+      <form className={styles.filters} onSubmit={handleSubmit}>
         <div className={styles.filter}>
           <input
             type="text"
             name="title"
-            onChange={() => {}}
+            onChange={handleChange}
             placeholder="Product name"
-            value={params.title}
+            value={values.title}
           />
         </div>
         <div className={styles.filter}>
           <input
             type="number"
             name="price_min"
-            onChange={() => {}}
+            onChange={handleChange}
             placeholder="0"
-            value={params.price_min}
+            value={values.price_min}
           />
+          <span>Price from</span>
         </div>
         <div className={styles.filter}>
           <input
             type="number"
             name="price_max"
-            onChange={() => {}}
+            onChange={handleChange}
             placeholder="0"
-            value={params.price_max}
+            value={values.price_max}
           />
+          <span>Price to</span>
         </div>
+
+        <button type="submit" hidden />
       </form>
+      {isLoading ? (
+        <div className="preloader">Loading...</div>
+      ) : !isSuccess || !items.length ? (
+        <div className={styles.back}>
+          <span>No result</span>
+          <button>Reset</button>
+        </div>
+      ) : (
+        <Products
+          title=""
+          products={items}
+          style={{ padding: 0 }}
+          amount={items.length}
+        />
+      )}
+
+      <div className={styles.more}>
+        {!isEnd && (
+          <button
+            onClick={() =>
+              setParams({ ...params, offset: params.offset + params.limit })
+            }
+          >
+            See more
+          </button>
+        )}
+      </div>
     </section>
   );
 };
